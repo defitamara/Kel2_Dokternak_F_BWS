6673fd1616e6fb8b7a741c0052c7056a4852ba58
Widya Yuristika Oktavia <yuristikawidya@gmail.com>
2022-05-20 18:06:52 +0700

    update

--- dokternak/resources/views/backend/data_user_staf/index.blade.php
+++ dokternak/resources/views/backend/data_user_staf/index.blade.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <meta name="robots" content="noindex,nofollow" />
-    <title>Data Staf IT</title>
+    <title>Data User Staf IT</title>
     <!-- Favicon icon -->
     <link
       rel="icon"
