090d3df5148d299b0f395ccd67120250795defa4
WikeeSriW <71832042+WikeeSriW@users.noreply.github.com>
2022-06-01 13:55:59 +0700

    Update ProfilController.php

--- dokternak/app/Http/Controllers/frontend/ProfilController.php
+++ dokternak/app/Http/Controllers/frontend/ProfilController.php
@@ -10,6 +10,10 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfilController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     
     public function index()
     {
@@ -24,11 +28,63 @@ class ProfilController extends Controller
 
     public function edit($id)
     {
-        $profil = DB::table('peternak')->where('id_peternak', $id)->first();
+        $id = Auth::id();
+        $profil = PeternakUser::select('peternak.*','users.*')
+        ->join('users', 'users.id', '=', 'peternak.id')
+        ->where('peternak.id', $id)->first();
         return view('frontend.editprofil',compact('profil'));
         //return dd($profil);
     }
-    public function update(Request $request , $id)
+    public function update(Request $request, $id)
+    {
+        // DB::table('users')->where('id',$request->id)->update([
+        //     'name' => $request->name,
+        //     'username' => $request->username,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request['password']),
+        // ]);
+
+        $message = [
+            'required' => ':attribute wajib diisi!!!',
+            'min' => ':attribute harus diisi minimal 15 huruf!!!',
+            'max' => ':attribute URL harus diisi maksimal 100 huruf!!!',
+            'mimes' => ':attribute harus berupa gambar dengan format (JPEG, PNG, dan SVG)',
+        ];
+
+        $gbr=$request->nama_gambar;
+        // if (isset($request->gambar) == NULL){
+        if($request->has('foto_peternak')) {
+            $getimageName = time().'.'.$request->foto_peternak->getClientOriginalExtension();
+            $request->foto_peternak->move(public_path('data/data_peternak'), $getimageName);
+        }else {
+            $getimageName = $gbr;
+        }
+        
+
+
+        $data_simpan = [
+            'namadepan_peternak' => $request->namadepan_peternak,
+            'namabelakang_peternak' => $request->namabelakang_peternak,
+            'no_hp' => $request->no_hp,
+            'jenis_kelamin' => $request->jenis_kelamin,
+            'alamat' => $request->alamat,
+            'foto_peternak' => $getimageName,
+        ];
+
+        PeternakUser::where('id_peternak', $id)->update($data_simpan);
+        
+        return redirect()->route('profil.index')
+                        ->with('success','Data peternak telah berhasil diperbarui');
+    }
+
+    public function lengkapiedit($id)
+    {
+        // $profil = DB::table('peternak')->where('id_peternak', $id)->first();
+        $profil = DB::table('users')->where('id', $id)->first();
+        return view('frontend.lengkapiprofil',compact('profil'));
+        //return dd($profil);
+    }
+    public function lengkapiupdate(Request $request , $id)
     {
         // DB::table('users')->where('id',$request->id)->update([
         //     'name' => $request->name,
